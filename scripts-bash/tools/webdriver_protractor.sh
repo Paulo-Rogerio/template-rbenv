588e3ae21509157d72845aa5290cdac3f37ed23a
Paulo Rogerio <paulo.gomes@al.go.leg.br>
2018-07-31 11:57:06 -0300

    Ajuste webdriver

--- scripts-bash/tools/webdriver_protractor.sh
+++ scripts-bash/tools/webdriver_protractor.sh
@@ -4,7 +4,7 @@ which webdriver-manager >/dev/null
 
 if [[ $? -eq 0 ]]
 then
-    echo "Yarn instalado: $(webdriver-manager version)"
+    echo "WebDriver instalado: $(webdriver-manager version)"
 else
     npm install --unsafe-perm --save-exact -g protractor@5.0.0 && npm update && webdriver-manager update
     export NODE_PATH=/usr/lib/node_modules
