98af7032882791670c982f5b6a3fac6e8d97e41a
Paulo Rogerio <paulo.gomes@al.go.leg.br>
2018-08-01 12:23:35 -0300

    Ajuste no caminho do diretorio onde rbenv é instalado

--- scripts-bash/tools/rbenv.sh
+++ scripts-bash/tools/rbenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ## Install rbenv
-export RBENV_ROOT="/usr/local/rbenv"
+export RBENV_ROOT="/root/.rbenv"
 
 if [[ ! -e ${RBENV_ROOT} ]]
 then
     mkdir -p ${RBENV_ROOT}	
     git clone https://github.com/rbenv/rbenv.git ${RBENV_ROOT}
-    export PATH=${RBENV_ROOT}/bin:${RBENV_ROOT}/shims:${PATH}
+    export PATH=${RBENV_ROOT}/bin:${RBENV_ROOT}/shims:$PATH
     cd ${RBENV_ROOT} && src/configure && make -C src
 
     ## Install ruby-build
@@ -17,16 +17,8 @@ then
     # Rbenv PATH (Usuário sistemas)
     echo "# Rbenv PATH" >> ~/.bashrc
     echo "export RBENV_ROOT="/usr/local/rbenv"" >> ~/.bashrc
-    echo 'export PATH=${RBENV_ROOT}/bin:${RBENV_ROOT}/libexec:${RBENV_ROOT}/shims:${RBENV_ROOT}/plugins/ruby-build/bin:$PATH' >> ~/.bashrc
+    echo 'export PATH=$RBENV_ROOT/bin:$RBENV_ROOT/libexec:$RBENV_ROOT/shims:$RBENV_ROOT/plugins/ruby-build/bin:$PATH' >> ~/.bashrc
     echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-
-    # Rbenv PATH Global
-    echo "" > /etc/profile.d/rbenv.sh
-    echo "# Rbenv PATH" >> /etc/profile.d/rbenv.sh
-    echo "export RBENV_ROOT="/usr/local/rbenv"" >> /etc/profile.d/rbenv.sh
-    echo 'export PATH=${RBENV_ROOT}/bin:${RBENV_ROOT}/shims:${RBENV_ROOT}/plugins/ruby-build/bin:$PATH' >> /etc/profile.d/rbenv.sh
-    echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
-    chmod +x /etc/profile.d/rbenv.sh
-    source /etc/profile.d/rbenv.sh    
+    source ~/.bashrc
 
 fi    
